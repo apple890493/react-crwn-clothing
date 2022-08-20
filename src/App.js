14c5d823a3d0a6b796e648eeae13a0238e83b69b
nono <nono@gitlab.com>
2022-08-20 15:46:20 +0800

    create directory component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import './categories.style.scss';
-
-import CategoryItem from './components/category-item/category-item.component'
+import Directory from './components/directory/directory.component'
 
 function App() {
   const categories = [
@@ -32,12 +30,7 @@ function App() {
   ];
 
   return (
-
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div >
+    <Directory categories={categories} />
   );
 }
 
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import './directory.style.scss';
+import CategoryItem from '../category-item/category-item.component'
+
+function Directory({ categories }) {
+
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div >
+  )
+}
+
+export default Directory;
