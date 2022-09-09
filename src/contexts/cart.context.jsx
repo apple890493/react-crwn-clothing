0c84583742e2cff8b6900203c756e64be793b08a
nonoLin <denknono@gmail.com>
2022-09-09 21:33:20 +0800

    used useReducer instead of useState

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState, useEffect } from 'react';
-
+import { createContext, useState, useEffect, useReducer } from 'react';
+import { createAction } from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
   //find items in cartItems
@@ -39,52 +39,112 @@ export const CartContext = createContext({
   cartTotal: 0
 });
 
-/*
-  product {
-    id
-    name
-    price
-    imageUrl
-  }
+const CART_ACTION_TYPES = {
+  SET_IS_CART_OPEN: 'SET_IS_CART_OPEN',
+  SET_CART_ITEMS: 'SET_CART_ITEMS',
+  // SET_CART_COUNT: 'SET_CART_COUNT',
+  // SET_CART_TOTAL: 'SET_CART_TOTAL',
+};
 
-  cart Item {
-    id
-    name
-    price
-    imageUrl
-    quantity // s
+const INITAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  //reducer should not hadle any logic
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload
+      }
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload
+      }
+    default:
+      throw new Error(`error in type ${type} in cartReducer`)
   }
-*/
+}
 
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setCartIsOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
+  // const [isCartOpen, setCartIsOpen] = useState(false);
+  // const [cartItems, setCartItems] = useState([]);
+  // const [cartCount, setCartCount] = useState(0);
+  // const [cartTotal, setCartTotal] = useState(0);
+  const [{ cartItems, cartTotal, cartCount, isCartOpen }, dispatch] = useReducer(cartReducer, INITAL_STATE);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const newCartCount = cartItems.reduce((total, curr) => total + curr.quantity, 0);
+  //   setCartCount(newCartCount);
+  // }, [cartItems]); //在這邊因為需要[]中的cartItems有更新資料才需要更新總數量，使用useEffect會比useState更方便適合
+
+  // useEffect(() => {
+  //   const newCartTotal = cartItems.reduce((total, curr) => total + curr.quantity * curr.price, 0);
+  //   setCartTotal(newCartTotal);
+  // }, [cartItems]);
+
+  const updateCartItemsReducer = (newCartItems) => {
+    //
+    /*
+      dispatch new action with payload = {
+        newCartItems,
+        newCartTotal,
+        newCartCount
+      }
+    */
+
     const newCartCount = cartItems.reduce((total, curr) => total + curr.quantity, 0);
-    setCartCount(newCartCount);
-  }, [cartItems]); //在這邊因為需要[]中的cartItems有更新資料才需要更新總數量，使用useEffect會比useState更方便適合
-
-  useEffect(() => {
     const newCartTotal = cartItems.reduce((total, curr) => total + curr.quantity * curr.price, 0);
-    setCartTotal(newCartTotal);
-  }, [cartItems]);
+
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+        cartItems: newCartItems,
+        cartTotal: newCartTotal,
+        cartCount: newCartCount
+      }))
+  };
 
   const addItemToCart = (productToAdd) => {
-    setCartItems(addCartItem(cartItems, productToAdd));
+    //setCartItems(addCartItem(cartItems, productToAdd));
+    const newCartItems = addCartItem(cartItems, productToAdd);
+    updateCartItemsReducer(newCartItems);
   };
 
   const removeItemfromCart = (productToRemove) => {
-    setCartItems(removeCartItem(cartItems, productToRemove));
+    //setCartItems(removeCartItem(cartItems, productToRemove));
+    const newCartItems = removeCartItem(cartItems, productToRemove);
+    updateCartItemsReducer(newCartItems);
   };
 
   const clearItemfromCart = (productToClear) => {
-    setCartItems(clearCartItem(cartItems, productToClear));
+    //setCartItems(clearCartItem(cartItems, productToClear));
+    const newCartItems = clearCartItem(cartItems, productToClear);
+    updateCartItemsReducer(newCartItems);
   };
 
-  const value = { isCartOpen, setCartIsOpen, addItemToCart, cartItems, cartCount, removeItemfromCart, clearItemfromCart, cartTotal };
+  const setCartIsOpen = (bool) => {
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool)
+    )
+  };
+
+  const value = {
+    isCartOpen,
+    setCartIsOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+    removeItemfromCart,
+    clearItemfromCart,
+    cartTotal
+  };
 
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,15 +1,51 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 
+// context still need it, caz vlaue that we expose
 export const UserContext = createContext({
   //這邊需要儲存一個defalut值;
   currentUser: null,
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPE = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+};
+
+const userReducer = (state, action) => {
+  // console.log('state', state, 'action', action)
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPE.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload
+      }
+    default:
+      throw new Error(`error in type ${type} in userReducer`)
+  }
+}
+
+const INITAL_STATE = { currentUser: {} };
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  //const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITAL_STATE);
+  // console.log('currentUser', currentUser)
+  const setCurrentUser = (user) => {
+    dispatch(
+      createAction(USER_ACTION_TYPE.SET_CURRENT_USER, user)
+      //createAction instead of origin  {
+      //   type: USER_ACTION_TYPE.SET_CURRENT_USER,
+      //   payload: user
+      // }
+    )
+  };
+
   const value = { currentUser, setCurrentUser };
+
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       // console.log(user)
@@ -31,4 +67,20 @@ export const UserProvider = ({ children }) => {
   </UserContext.Provider>
 
   ** Provider用來改變value；useContext用來傳遞value
+*/
+
+/*
+範例利用 Reducer
+  const userReducer = (state, action) => {
+    //根據stata, action 來返回對象
+    return {
+      currentUser:
+    }
+  }
+*/
+
+/*
+兩者不同的地方在於
+useState()是整個變化
+useReducer是根據狀態與動作變化
 */
